e2fc49857e193b3d8619536d12ac3a24b8dd8f84
Yiannis <yntses@gmail.com>
2024-03-22 10:55:11 +0200

    start

---
+++ lib1/src/test/java/gr/tses/lib1/test/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package gr.tses.lib1.test;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends RuleTest {
+    // Execute the same tests but in packaged mode.
+}
---
+++ service/src/test/java/gr/tses/service/test/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package gr.tses.service.test;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
